8e8b2b6de2901b612fa5aa6f016f513e2190330f
elisei <ge94jip@mytum.de>
2023-06-10 15:26:42 +0200

    qwq

--- src/gad/doublehashing/DoubleHashInt.java
+++ src/gad/doublehashing/DoubleHashInt.java
@@ -1,19 +1,40 @@
 package gad.doublehashing;
 
 public class DoubleHashInt implements DoubleHashable<Integer> {
-	private int primSize;
+	private int m;
+	private int prime;
 
 	public DoubleHashInt(int primeSize) {
-		this.primSize= primSize;
+		m = findNextPrime(primeSize);
+		prime = findNextPrime(m - 1);
 	}
+
+	private int findNextPrime(int n) {
+		while (!isPrime(n)) {
+			n++;
+		}
+		return n;
+	}
+	private boolean isPrime(int n) {
+		if (n <= 1) {
+			return false;
+		}
+		for (int i = 2; i * i <= n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 
 	@Override
 	public int hash(Integer key) {
-		return key % primSize;
+		return key % m;
 	}
 
 	@Override
 	public int hashTick(Integer key) {
-		return (1 + key) % primSize - 1;
+		return prime - (key % prime);
 	}
 }
