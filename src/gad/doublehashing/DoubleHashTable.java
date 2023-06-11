0eddc139e4287c5f1d6c94118755b7f5692d024e
ge94jip <ge94jip@mytum.de>
2023-06-11 15:07:16 +0200

    qwq

--- src/gad/doublehashing/DoubleHashTable.java
+++ src/gad/doublehashing/DoubleHashTable.java
@@ -3,10 +3,18 @@ package gad.doublehashing;
 import java.util.Optional;
 
 public class DoubleHashTable<K, V> {
-
+	private Pair<K, V>[] table;
+	private int size;
+	private int collisions;
+	private int maxRehashes;
+	private DoubleHashable<K> hashable;
 	@SuppressWarnings("unchecked")
 	public DoubleHashTable(int primeSize, HashableFactory<K> hashableFactory) {
-		// Erstellt ein Array von Pairs: (Pair<K, V>[]) new Pair[primeSize];
+		size = primeSize;
+		table = (Pair<K, V>[]) new Pair[size];
+		collisions = 0;
+		maxRehashes = 0;
+		hashable = hashableFactory.create(size);
 	}
 
 	public int hash(K key, int i) {
@@ -14,7 +22,25 @@ public class DoubleHashTable<K, V> {
 	}
 
 	public boolean insert(K k, V v) {
-		return false;
+		int rehashes = 0;
+		int index = hash(k, rehashes);
+
+		while (table[index] != null && !table[index].one().equals(k)) {
+			rehashes++;
+			index = hash(k, rehashes);
+		}
+
+		if (table[index] != null) {
+			table[index].two();
+		} else {
+			table[index] = new Pair<>(k, v);
+		}
+
+		if (rehashes > maxRehashes) {
+			maxRehashes = rehashes;
+		}
+
+		return true;
 	}
 
 	public Optional<V> find(K k) {
